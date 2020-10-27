621a411373f81bf57b18034d41dc8d28dcfa9e49
vicky1999 <ravivignesh2000@gmail.com>
2020-10-27 21:32:58 +0530

    Shift Cipher Documentation

--- src/Cipher/ShiftCipher.js
+++ src/Cipher/ShiftCipher.js
@@ -2,6 +2,26 @@ const { charCodeAt } = require('../Alphabets');
 const alphabets=require('../Alphabets');
 
 const ShiftCipher = (text, shift) => {
+    /**
+     * 
+     * Shifts each alphabets in the plain text by a shift amount.
+     * 
+     * Syntax:
+     *  ShiftCipher(PlainText,ShiftValue)
+     * 
+     * Parameters: 
+     *  text: String     // Plain Text for cipher
+     *  shift: Integer   //The number of Shifts needed for each letters in the plain text
+     * 
+     * Returns:
+     *  Shift Cipher for the plain text
+     * 
+     * Example:
+     *  ShiftCipher("ABCD",3) --> returns DEFG
+     *  ShiftCipher("AbCd",1) --> returns BcDe
+     * 
+     */
+
     cipher="";
     for(var i=0; i<text.length; i++) {
         var index=(alphabets.indexOf(text[i])+shift) % 26;
