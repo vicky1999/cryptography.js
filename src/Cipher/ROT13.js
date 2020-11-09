c93214ace5dd212a432dbefc1b83584a6e58e5cc
vicky1999 <ravivignesh2000@gmail.com>
2020-11-09 23:22:50 +0530

    Documentation for ROT13 created

--- src/Cipher/ROT13.js
+++ src/Cipher/ROT13.js
@@ -1,4 +1,26 @@
 const ROT13 = (text) => {
+    /**
+     * 
+     * rotates each character in the plain text by 13 letters.
+     * 
+     * Syntax:
+     *  ROT13(PlainText)
+     * 
+     * Parameters: 
+     *  text: String     // Plain Text for cipher
+     * 
+     * Requirements:
+     *  The plain text should contain only alphabets or spaces
+     * 
+     * Returns:
+     *  ROT13 Cipher for the plain text with all upper case alphabets
+     * 
+     * Example:
+     *  ROT13("HELLOWORLD")      --> returns URYYBJBEYQ
+     *  ROT13("IAMPLAINTEXT")    --> returns VNZCYNVAGRKG
+     * 
+     */
+
     text=text.toUpperCase();
     let cipher='';
     for(var i=0;i<text.length;i++) {
