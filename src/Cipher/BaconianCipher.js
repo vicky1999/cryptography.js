cde1ced1c9675e73aba641f41274c0862883fc7e
vicky1999 <ravivignesh2000@gmail.com>
2020-11-11 09:56:56 +0530

    Bacon cipher implemented

---
+++ src/Cipher/BaconianCipher.js
@@ -0,0 +1,23 @@
+const alphabets=require('../Alphabets');
+
+const BaconCipher = (text) => {
+    text=text.toUpperCase();
+    let cipher='';
+    for(var i=0;i<text.length;i++) {
+        if(text.charCodeAt(i)>=65 && text.charCodeAt(i)<=90) {
+            let val=text.charCodeAt(i)-65;
+            val=val.toString(2);
+            for(let j=val.length;j<5;j++) {
+                cipher+='a';
+            }
+            for(let j=0;j<val.length;j++) {
+                if(val[j]=='0') cipher+='a';
+                else cipher+='b';
+            }
+        }
+        else cipher+=text[i];
+    }
+    return cipher;
+}
+
+module.exports=BaconCipher;
