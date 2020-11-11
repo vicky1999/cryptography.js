b16cc9dce3a5b3e87522fc3c790a84816efb8c48
vicky1999 <ravivignesh2000@gmail.com>
2020-11-11 08:36:12 +0530

    documentation for atbash cipher done

--- src/Cipher/AtbashCipher.js
+++ src/Cipher/AtbashCipher.js
@@ -1,6 +1,25 @@
 const alphabets=require('../Alphabets');
 
 const AtbashCipher = (text) => {
+    /**
+     * 
+     * the letters of the alphabet are reversed.
+     * 
+     * Syntax:
+     *  AtbashCipher(PlainText)
+     * 
+     * Parameters: 
+     *  text: String     // Plain Text for cipher
+     * 
+     * Returns:
+     *  Atbash Cipher for the plain text with all upper case alphabets
+     * 
+     * Example:
+     *  AtbashCipher("HELLOWORLD")      --> returns SVOOLDLIOW
+     *  AtbashCipher("IAMPLAINTEXT")    --> returns RZNKOZRMGVCG
+     * 
+     */
+
     let cipher='';
     text=text.toUpperCase();
     for(var i=0;i<text.length;i++) {
