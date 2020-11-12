28ece09a9435ed683259644a19552dd16857bf23
vicky1999 <ravivignesh2000@gmail.com>
2020-11-12 16:12:33 +0530

    rail fence cipher bundled

--- src/bundle.js
+++ src/bundle.js
@@ -7,6 +7,7 @@ module.exports.BaconCipher=require('./Cipher/BaconianCipher');
 module.exports.DES=require('./Cipher/DES');
 module.exports.OneTimePad=require('./Cipher/OneTimePad');
 module.exports.PlayFairCipher=require('./Cipher/PlayFairCipher');
+module.exports.RailFenceCipher=require('./Cipher/RailFenceCipher');
 module.exports.ROT13=require('./Cipher/ROT13');
 module.exports.ShiftCipher=require('./Cipher/ShiftCipher');
 module.exports.SubstitutionCipher=require('./Cipher/SubstitutionCipher');
