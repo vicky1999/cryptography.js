ba1114516504e822b2147f18876b71916a8a49e4
vicky1999 <ravivignesh2000@gmail.com>
2020-11-14 21:01:17 +0530

    Documentation for Auto Key cipher added

--- src/Cipher/AutoKeyCipher.js
+++ src/Cipher/AutoKeyCipher.js
@@ -1,6 +1,26 @@
 const { VigenereCipher } = require('../bundle');
 
 const AutoKeyCipher=(text,key) => {
+    /**
+     * 
+     * Encrypts alphabetic data using a table called Vigenere Table but uses different method to generate key.
+     * 
+     * Syntax:
+     *  AutoKeyCipher(PlainText,key)
+     * 
+     * Parameters: 
+     *  text: String     // Plain Text for cipher
+     *  key: String      // key value for encryption
+     * 
+     * Returns:
+     *  Vigenere Cipher for the plain text with all upper case alphabets
+     * 
+     * Example:
+     *  AutoKeyCipher("HELLOWORLD","HELLO")         --> returns OIWWCDSCWR
+     *  AutoKeyCipher("IAMPLAINTEXT","CRYPTO")    --> returns KRKEEOQNFTIT
+     * 
+     */
+
     let ind=0;
     for(var i=key.length;i<text.length;i++) {
         key+=text[ind];
@@ -10,4 +30,4 @@ const AutoKeyCipher=(text,key) => {
     return cipher;
 }
 
-module.exports=AutoKeyCipher;
+module.exports = AutoKeyCipher;
