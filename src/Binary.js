9c89fca6776f35602d51041f9c1d917dc78747c6
vicky1999 <ravivignesh2000@gmail.com>
2020-11-17 21:41:32 +0530

    Documentation for Binary class added

--- src/Binary.js
+++ src/Binary.js
@@ -1,4 +1,21 @@
 class Binary {
+    /**
+     * 
+     * Conversion of data from Binary to Text, Decimal, Octal, and Hexadecimal.
+     * 
+     * Parameters:
+     *  val: String  representing the binary value
+     * 
+     * Returns: 
+     *  String: Decimal, Octal and Hexadecimal representations of the binary data.
+     * Eg.:
+     *     Binary.toDecimal("1010") ==> returns '10'
+     *     Binary.toHex("1010") ==> returns 'a'
+     *     Binary.toOctal("1010") ==> returns '12'
+     *     Binary.toText("01100001") ==> returns 'a'
+     * 
+     * 
+     */
     static toDecimal(val) {
         return parseInt(val,2).toString(10);
     }
