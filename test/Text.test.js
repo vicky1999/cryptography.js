ad7c08c8ecb290460ff3fb10f5d0242ae6dbdce6
vicky1999 <ravivignesh2000@gmail.com>
2020-11-22 00:33:17 +0530

    Unit tests for text conversion done

---
+++ test/Text.test.js
@@ -0,0 +1,17 @@
+const assert=require('assert');
+const { Text } = require('../src/bundle');
+
+describe("Text conversion Test", () => {
+    it("Text to Decimal", () => {
+        assert.strictEqual(Text.toDecimal('A'),'65');
+    });
+    it("Text to octal", () => {
+        assert.strictEqual(Text.toOctal('A'),'101');
+    });
+    it("Text to Hexadecimal", () => {
+        assert.strictEqual(Text.toHex('A'),'41');
+    });
+    it("Text to Binary", () => {
+        assert.strictEqual(Text.Binary('A'),'1000001');
+    });
+})
