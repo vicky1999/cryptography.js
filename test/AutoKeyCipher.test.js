c723a24f01210e4e9ca4199778c3fbb81c3f89b0
vicky1999 <ravivignesh2000@gmail.com>
2020-12-03 09:51:33 +0530

    autokey cipher class created

--- test/AutoKeyCipher.test.js
+++ test/AutoKeyCipher.test.js
@@ -3,15 +3,15 @@ const { AutoKeyCipher } = require('../src/bundle');
 
 describe("Auto Key Cipher Test", () => {
     it("Text with spaces", () => {
-        assert.strictEqual(AutoKeyCipher("HELLO WORLD","HELLO"),"OIWWCDSCWR");
+        assert.strictEqual(AutoKeyCipher.encrypt("HELLO WORLD","HELLO"),"OIWWCDSCWR");
     });
     it("Text with different cases", () => {
-        assert.strictEqual(AutoKeyCipher("I am plain text","CRYPTO"),"KRKEEOQNFTIT");
+        assert.strictEqual(AutoKeyCipher.encrypt("I am plain text","CRYPTO"),"KRKEEOQNFTIT");
     });
     it("Text with same cases without spaces", () => {
-        assert.strictEqual(AutoKeyCipher("IAMPLAINTEXT","CRYPTO"),"KRKEEOQNFTIT");
+        assert.strictEqual(AutoKeyCipher.encrypt("IAMPLAINTEXT","CRYPTO"),"KRKEEOQNFTIT");
     });
     it("Text with lowercase cases without spaces", () => {
-        assert.strictEqual(AutoKeyCipher("iamplaintext","crypto"),"KRKEEOQNFTIT");
+        assert.strictEqual(AutoKeyCipher.encrypt("iamplaintext","crypto"),"KRKEEOQNFTIT");
     });    
 })
