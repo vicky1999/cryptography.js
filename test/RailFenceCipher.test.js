37a02bd974192f1e899d58a94b0063e22da9d8ac
vicky1999 <ravivignesh2000@gmail.com>
2020-12-12 08:10:41 +0530

    unit tests for rail fence cipher corrected

--- test/RailFenceCipher.test.js
+++ test/RailFenceCipher.test.js
@@ -1,17 +1,17 @@
 const assert=require('assert');
-const RailFenceCipher=require('../src/Cipher/RailFenceCipher');
+const RailFenceCipher=require('../src/bundle');
 
 describe("Rail Fence Cipher Test", () => {
     it("Text with spaces", () => {
-        assert.strictEqual(RailFenceCipher("HELLO WORLD",3),"HOLELWRDLO");
+        assert.strictEqual(RailFenceCipher.encrypt("HELLO WORLD",3),"HOLELWRDLO");
     });
     it("Text with different cases", () => {
-        assert.strictEqual(RailFenceCipher("Hello World",3),"HolelWrdlo");
+        assert.strictEqual(RailFenceCipher.encrypt("Hello World",3),"HolelWrdlo");
     });
     it("Text with same cases without spaces", () => {
-        assert.strictEqual(RailFenceCipher("IAMPLAINTEXT",3),"ILTAPANETMIX");
+        assert.strictEqual(RailFenceCipher.encrypt("IAMPLAINTEXT",3),"ILTAPANETMIX");
     });
     it("Text with lowercase cases without spaces", () => {
-        assert.strictEqual(RailFenceCipher("iamplaintext",3),"iltapanetmix");
+        assert.strictEqual(RailFenceCipher.encrypt("iamplaintext",3),"iltapanetmix");
     });    
 })
