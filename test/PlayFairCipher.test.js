86704fbf82d9367b3d9ad13f60fc9af314bd08d4
vicky1999 <ravivignesh2000@gmail.com>
2020-12-12 07:45:15 +0530

    Playfair cipher code and tests corrected

--- test/PlayFairCipher.test.js
+++ test/PlayFairCipher.test.js
@@ -3,13 +3,13 @@ const PlayFairCipher=require('../src/Cipher/PlayFairCipher');
 
 describe("Playfair Cipher Test", () => {
     it("PlayFair Cipher Test", () => {
-        assert.strictEqual(PlayFairCipher("PLAYFAIRCIPHER","ENCRYPTION"),"TKHAHIOCIFABNY");
+        assert.strictEqual(PlayFairCipher.encrypt("PLAYFAIRCIPHER","ENCRYPTION"),"TKHAHIOCIFABNY");
     });
     it("Playfair Cipher with different cases", () => {
-        assert.strictEqual(PlayFairCipher("PlayFairCipher","Encryption"),"TKHAHIOCIFABNY");
+        assert.strictEqual(PlayFairCipher.encrypt("PlayFairCipher","Encryption"),"TKHAHIOCIFABNY");
     });
     it("Playfair Cipher Test", () => {
-        assert.strictEqual(PlayFairCipher("PROGRAMMING","HELLOWORLD"),"NDEKDBIZFQMV");
+        assert.strictEqual(PlayFairCipher.encrypt("PROGRAMMING","HELLOWORLD"),"NDEKDBIZFQMV");
     });
     
 })
