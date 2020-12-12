c1973449b8f55eebd0536a642474de28c5be1072
vicky1999 <ravivignesh2000@gmail.com>
2020-12-12 07:52:17 +0530

    polybius cipher code, documentation and tests corrected

--- src/Cipher/PolybiusCipher.js
+++ src/Cipher/PolybiusCipher.js
@@ -1,38 +1,40 @@
-const PolybiusCipher=(text,key) => {
-    /**
-     * 
-     * each plaintext character is encrypted as 2 ciphertext characters.
-     * 
-     * Syntax:
-     *  PolybiusCipher(PlainText,key)
-     * 
-     * Parameters: 
-     *  text: String     // Plain Text for cipher
-     *  key: String      // key value for encryption
-     * 
-     * Requirements:
-     *  The length of the key should be the 25 characters.
-     * 
-     * Returns:
-     *  Polybius Square Cipher for the plain text with all upper case alphabets.  The alphabets in the cipher text were one of A, B, C, D, E.
-     * 
-     * Example:
-     *  PolybiusCipher("hello","ectbkdlanqwgyoirvmsxpfuhz") ---> EDAABBBBCD
-     *  PolybiusCipher("IAMPLAINTEXT","ectbkdlanqwgyoirvmsxpfuhz") ---> CEBCDCEABBBCCEBDACAADEAC
-     * 
-     */
+class PolybiusCipher {
+    static encrypt(text,key) {
+        /**
+         * 
+         * each plaintext character is encrypted as 2 ciphertext characters.
+         * 
+         * Syntax:
+         *  PolybiusCipher.encrypt(PlainText,key)
+         * 
+         * Parameters: 
+         *  text: String     // Plain Text for cipher
+         *  key: String      // key value for encryption
+         * 
+         * Requirements:
+         *  The length of the key should be the 25 characters.
+         * 
+         * Returns:
+         *  Polybius Square Cipher for the plain text with all upper case alphabets.  The alphabets in the cipher text were one of A, B, C, D, E.
+         * 
+         * Example:
+         *  PolybiusCipher.encrypt("hello","ectbkdlanqwgyoirvmsxpfuhz") ---> EDAABBBBCD
+         *  PolybiusCipher.encrypt("IAMPLAINTEXT","ectbkdlanqwgyoirvmsxpfuhz") ---> CEBCDCEABBBCCEBDACAADEAC
+         * 
+         */
 
-    key=key.toUpperCase();
-    text=text.replace(/[^A-Za-z]/g ,"").toUpperCase();
-    let cipher='';
-    for(var i=0;i<text.length;i++) {
-        let ind=key.indexOf(text[i]);
-        if(ind===-1) ind=key.indexOf("I");
-        let row=Math.floor(ind/5);
-        let col=ind%5;
-        cipher+=String.fromCharCode(65+row)+String.fromCharCode(65+col);
+        key=key.toUpperCase();
+        text=text.replace(/[^A-Za-z]/g ,"").toUpperCase();
+        let cipher='';
+        for(var i=0;i<text.length;i++) {
+            let ind=key.indexOf(text[i]);
+            if(ind===-1) ind=key.indexOf("I");
+            let row=Math.floor(ind/5);
+            let col=ind%5;
+            cipher+=String.fromCharCode(65+row)+String.fromCharCode(65+col);
+        }
+        return cipher;
     }
-    return cipher;
 }
 
 module.exports=PolybiusCipher;
--- test/PolybiusCipher.test.js
+++ test/PolybiusCipher.test.js
@@ -3,15 +3,15 @@ const { PolybiusCipher } = require('../src/bundle');
 
 describe("Polybius Square Cipher Test", () => {
     it("Text with spaces", () => {
-        assert.strictEqual(PolybiusCipher("hello world","ectbkdlanqwgyoirvmsxpfuhz"),"EDAABBBBCDCACDDABBBA");
+        assert.strictEqual(PolybiusCipher.encrypt("hello world","ectbkdlanqwgyoirvmsxpfuhz"),"EDAABBBBCDCACDDABBBA");
     });
     it("Text with different cases", () => {
-        assert.strictEqual(PolybiusCipher("Hello","ectbkdlanqwgyoirvmsxpfuhz"),"EDAABBBBCD");
+        assert.strictEqual(PolybiusCipher.encrypt("Hello","ectbkdlanqwgyoirvmsxpfuhz"),"EDAABBBBCD");
     });
     it("Text with same cases without spaces", () => {
-        assert.strictEqual(PolybiusCipher("IAMPLAINTEXT","ectbkdlanqwgyoirvmsxpfuhz"),"CEBCDCEABBBCCEBDACAADEAC");
+        assert.strictEqual(PolybiusCipher.encrypt("IAMPLAINTEXT","ectbkdlanqwgyoirvmsxpfuhz"),"CEBCDCEABBBCCEBDACAADEAC");
     });
     it("Text with lowercase cases without spaces", () => {
-        assert.strictEqual(PolybiusCipher("iamplaintext","ectbkdlanqwgyoirvmsxpfuhz"),"CEBCDCEABBBCCEBDACAADEAC");
+        assert.strictEqual(PolybiusCipher.encrypt("iamplaintext","ectbkdlanqwgyoirvmsxpfuhz"),"CEBCDCEABBBCCEBDACAADEAC");
     });    
 })
