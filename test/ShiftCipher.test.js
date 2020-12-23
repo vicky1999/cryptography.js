fa2497f0d9a59d63b976b96884d5b4cf7838f0bb
vicky1999 <ravivignesh2000@gmail.com>
2020-12-23 13:38:11 +0530

    Shift cipher class created

--- test/ShiftCipher.test.js
+++ test/ShiftCipher.test.js
@@ -3,9 +3,9 @@ const ShiftCipher=require('../src/Cipher/ShiftCipher');
 
 describe("Shift Cipher Test", () => {
     it("Message with same cases", () => {
-        assert.strictEqual(ShiftCipher("ABCD",3),"DEFG");
+        assert.strictEqual(ShiftCipher.encrypt("ABCD",3),"DEFG");
     });
     it("Message with different cases", () => {
-        assert.strictEqual(ShiftCipher("HeLlO WoRlD",1),"IfMmP XpSmE");
+        assert.strictEqual(ShiftCipher.encrypt("HeLlO WoRlD",1),"IfMmP XpSmE");
     });
 })
