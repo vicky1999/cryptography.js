9ab33bfff470f8aa8245a8891e7d1f1485de0000
Vignesh <ravivignesh2000@gmail.com>
2021-01-05 15:14:37 +0000

    Transposition Cipher class created

--- test/TranspositionCipher.test.js
+++ test/TranspositionCipher.test.js
@@ -3,13 +3,13 @@ const TranspositionCipher=require('../src/Cipher/TranspositionCipher');
 
 describe("Transposition Cipher Test", () => {
     it("Text with same lengths", () => {
-        assert.strictEqual(TranspositionCipher("HELLO","IMKEY"),"LHLEO");
+        assert.strictEqual(TranspositionCipher.encrypt("HELLO","IMKEY"),"LHLEO");
     });
     it("Text with different lengths", () => {
-        assert.strictEqual(TranspositionCipher("HELLOWORLD","IAMKEY"),"ERO~HOLDLLW~");
+        assert.strictEqual(TranspositionCipher.encrypt("HELLOWORLD","IAMKEY"),"ERO~HOLDLLW~");
     });
     it("Text with different lengths", () => {
-        assert.strictEqual(TranspositionCipher("IAMPLAINTEXT","KEY"),"ALNXIPIEMATT");
+        assert.strictEqual(TranspositionCipher.encrypt("IAMPLAINTEXT","KEY"),"ALNXIPIEMATT");
     });
     
 })
